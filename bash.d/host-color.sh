a37703d7ed6e808f7086a60680914458d0e6deb3
Nathan Anderson <andnat@gmail.com>
2014-12-31 14:54:13 -0500

    Make laptop red

--- bash.d/host-color.sh
+++ bash.d/host-color.sh
@@ -1,6 +1,10 @@
 # Calculate a short checksum of the real hostname to determine a unique color
-if [[ $TERM =~ "256color" ]]; then
-   host_color="$(tput setaf $((16 + $(hostname | cksum | cut -c2-4) % 240)))";
+if [ $(hostname) == "mordor" ]; then
+  host_color="$(tput setaf 1)"
 else
-   host_color="$(tput setaf $((31 + $(hostname | cksum | cut -c2-4) % 6)))";
+  if [[ $TERM =~ "256color" ]]; then
+     host_color="$(tput setaf $((16 + $(hostname | cksum | cut -c2-4) % 240)))";
+  else
+     host_color="$(tput setaf $((31 + $(hostname | cksum | cut -c2-4) % 6)))";
+  fi
 fi
