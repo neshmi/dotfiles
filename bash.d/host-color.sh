064e5ef99e8bf3cb698d54a83a101ff08270cc88
Nathan Anderson <andnat@gmail.com>
2015-08-21 16:34:19 -0400

    Fix hostname for angband or linux. Probably will move this to an array sometime.

--- bash.d/host-color.sh
+++ bash.d/host-color.sh
@@ -1,12 +1,12 @@
 # Calculate a short checksum of the real hostname to determine a unique color
-if [ $(hostname) == "angband" ]; then
-  host_color="$(tput setaf 1)"
+if [ "$(hostname)" == "eriador" ] && [ "$(hostname)" == "angband" ]; then
+  export host_color="$(tput setaf 1)"
 else
-  if [[ $TERM =~ "256color" ]]; then
+  if [[ "$TERM" =~ 256color ]]; then
     # The weird '10#' in front of the hostname forces the number to be
     # interpreted as base10, not octal.
-    host_color="$(tput setaf $((16 + 10#$(hostname | cksum | cut -c2-4) % 256)))";
+    export host_color="$(tput setaf $((16 + 10#$(hostname | cksum | cut -c2-4) % 256)))";
   else
-    host_color="$(tput setaf $((31 + 10#$(hostname | cksum | cut -c2-4) % 6)))";
+    export host_color="$(tput setaf $((31 + 10#$(hostname | cksum | cut -c2-4) % 6)))";
   fi
 fi
