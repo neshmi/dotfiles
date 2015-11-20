812ec705e775d6b8da870b1da42172fb45549417
Nathan Anderson <andnat@gmail.com>
2015-11-20 06:39:27 -0500

    setup osx specific completions for brew

--- bash.d/brew-completion.sh
+++ bash.d/brew-completion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-if [ "$(which brew)x" != "x" ] && [ -f $(brew --prefix)/etc/bash_completion ]; then
+# Set up osx-specific completions
+if [[ $OSTYPE == darwin* ]] && [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
