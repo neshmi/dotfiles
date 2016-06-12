dba6c75f20f1c64167d44da453996df56748e48c
Nathan Anderson <andnat@gmail.com>
2016-06-12 08:22:31 -0500

    Tweak docker helpers

--- bash.d/docker.sh
+++ bash.d/docker.sh
@@ -1,2 +1,39 @@
-alias docker-clean-containers="docker rm $(docker ps -a -q)"
-alias docker-clean-images="docker rmi $(docker images -q)"
+function docker-stop-all(){
+  if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    docker stop $(docker ps -q)
+  fi
+}
+
+
+function docker-rm-containers(){
+  if [ $(docker ps -a -q | wc -l) -gt 0 ]; then
+    docker rm $(docker ps -a -q)
+  fi
+}
+
+function docker-rm-images(){
+  if [ $(docker images -q | wc -l) -gt 0 ]; then
+    docker rmi $(docker images -q)
+  fi
+}
+
+function docker-orphans(){
+ docker images | grep '<none>' | awk '{print $3}'
+}
+function docker-rm-orphans(){
+  if [ $(docker-orphans | wc -l) -gt 0 ]; then
+    docker rmi $(docker-orphans)
+  fi
+}
+
+function dsa(){
+  docker-stop-all
+}
+
+function drc(){
+  docker-rm-containers
+}
+
+function dri(){
+  docker-rm-images
+}
