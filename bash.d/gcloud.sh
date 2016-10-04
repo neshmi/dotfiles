208ae5ad19ab663e63dd46af1faa3118bd80d218
Nathan Anderson <andnat@gmail.com>
2016-10-04 19:00:49 -0400

    Add sublime prefs and fix gcloud init

--- bash.d/gcloud.sh
+++ bash.d/gcloud.sh
@@ -1,6 +1,6 @@
 
 # The next line updates PATH for the Google Cloud SDK.
-source '/Users/nathan/google-cloud-sdk/path.bash.inc'
+source '/Users/nathan/.google-cloud-sdk/path.bash.inc'
 
 # The next line enables shell command completion for gcloud.
-source '/Users/nathan/google-cloud-sdk/completion.bash.inc'
+source '/Users/nathan/.google-cloud-sdk/completion.bash.inc'
