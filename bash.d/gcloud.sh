9253dbd413915594a171fbffc39df3be7c8825cc
Nathan Anderson <andnat@gmail.com>
2016-11-08 15:32:34 -0500

    Fix gcloud assumption; gpg-agent deprecation; add fonts

--- bash.d/gcloud.sh
+++ bash.d/gcloud.sh
@@ -1,6 +1,7 @@
+if [ -e "~/.google-cloud-sdk" ]; then
+  # The next line updates PATH for the Google Cloud SDK.
+  source '~/.google-cloud-sdk/path.bash.inc'
 
-# The next line updates PATH for the Google Cloud SDK.
-source '/Users/nathan/.google-cloud-sdk/path.bash.inc'
-
-# The next line enables shell command completion for gcloud.
-source '/Users/nathan/.google-cloud-sdk/completion.bash.inc'
+  # The next line enables shell command completion for gcloud.
+  source '~/.google-cloud-sdk/completion.bash.inc'
+fi
