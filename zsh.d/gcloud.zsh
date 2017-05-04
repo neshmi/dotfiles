eca57216f1fe1dcc6cf7d2c8cf8aebedeead0cc0
Nathan Anderson <andnat@gmail.com>
2017-05-04 09:23:08 -0400

    Only source if present

---
+++ zsh.d/gcloud.zsh
@@ -0,0 +1,7 @@
+if [ -d ~/.gcloud ]; then
+  # The next line updates PATH for the Google Cloud SDK.
+  source ~/.gcloud/path.zsh.inc
+
+  # The next line enables zsh completion for gcloud.
+  source ~/.gcloud/completion.zsh.inc
+fi
