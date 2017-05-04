593905291864d3d425abe7ab14e920245791643a
Nathan Anderson <andnat@gmail.com>
2017-05-04 09:22:40 -0400

    Comment out for now. Ugh.

--- sh.d/gpg-agent.sh
+++ sh.d/gpg-agent.sh
@@ -1,9 +1,9 @@
-[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
-if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
-  export GPG_AGENT_INFO
-else
-  eval $( gpg-agent --daemon ~/.gpg-agent-info )
-fi
+#[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
+#if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
+#  export GPG_AGENT_INFO
+#else
+#  eval $( gpg-agent --daemon ~/.gpg-agent-info )
+#fi
 
 #. ~/.gpg-agent-info
 #export GPG_AGENT_INFO
