28d4010accbc568f317dfa5d48c6ecd52b35aa0c
Nathan Anderson <andnat@gmail.com>
2017-07-26 21:46:06 -0400

    Update zplug installer

--- setup.sh
+++ setup.sh
@@ -1,82 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -eo pipefail
 
-set -Eeuo pipefail
+if [ ! -d ~/.zplug ]; then
+  #curl -sL zplug.sh/installer | zsh
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+fi
 
-cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
-
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-usage() {
-  cat <<EOF
-Usage: $(basename "$0") [-h] [-v] [-f]
-
-Install prerequisites and configure dotfiles
-
-Available options:
-
--h, --help      Print this help and exit
--v, --verbose   Print script debug info
--f, --force     Force installation
--c, --cleanup   Uninstall/Cleanup dotfiles
-EOF
-  exit
-}
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
-}
-
-setup_colors() {
-  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOCOLOR='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-  else
-    NOCOLOR='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-  fi
-}
-
-msg() {
-  echo >&2 -e "${1-}"
-}
-
-die() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
-  msg "$msg"
-  exit "$code"
-}
-
-parse_params() {
-  # default values of variables set from params
-  force=""
-  verbose=""
-  cleanup=""
-  target="$HOME"
-
-  while :; do
-    case "${1-}" in
-    -h | --help)
-      usage
-      ;;
-    -v | --verbose)
-      set -x
-      verbose="-v"
-      ;;
-    --no-color)
-      NO_COLOR=1
-      ;;
-    -f | --force)
-      force="--restow"
-      ;;
-    -c | --cleanup)
-      cleanup="-D"
-      ;;
-    -?*)
-      die "Unknown option: $1"
-      ;;
-    *)
-      break
-      ;;
+for file in *
+do
+  echo -n "Processing $file"
+  #if [ ! -d $file ]; then
+    # It's just a file, link it
+    case $file in
+      # Skip setup files
+      setup.sh|bash_profile|flat-colors.json|flat.terminal|osx.sh|sublime.prefs|apply_colors.sh)
+        echo "      skipped"
+        ;;
+      *)
+        # Clean up the link to the directory
+        if [ -d ${file} ]; then
+          if [ -d ~/.${file} ]; then
+            rm ~/.${file}
+          fi
+        fi
+        # Link the new one in place
+        ln -sf $(pwd)/${file} ~/.${file}
+        echo "      linked"
+        ;;
     esac
     shift
   done
