888e6fc8ca073cc3f0f7279621078223beb84507
Nathan Anderson <andnat@gmail.com>
2018-01-19 12:11:22 -0500

    Fix ruby options in preparation for 2.5

--- sh.d/exports.sh
+++ sh.d/exports.sh
@@ -2,9 +2,6 @@
 # Change this to your console based IRC client of choice.
 export IRC_CLIENT='weechat'
 
-# Ruby default options
-export RUBYOPT="-Ilib -Itest -rubygems"
-
 # History Control
 export HISTTIMEFORMAT="%F %T "
 export HISTCONTROL="ignoreboth"
@@ -33,10 +30,6 @@ fi
 export TERM=xterm-256color
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
-
-# Enable color in grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
 
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
 export ARCHFLAGS='-arch x86_64'
---
+++ sh.d/ruby_exports.sh
@@ -0,0 +1,10 @@
+# Ruby:
+#
+# The -rubygems option breaks Ruby 2.5
+# export RUBYOPT="-Ilib -Itest -rubygems"
+#
+# Jeff says: "can also use RUBYLIB if you're just setting lib options"
+# export RUBYLIB=:./lib:./test:./test/lib:
+#
+# Don't need this any more?
+# export RUBY_CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline`"
