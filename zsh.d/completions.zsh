f5cf1ae13ccf1d8f0248ae56c98ebfbec1ea4306
Nathan Anderson <andnat@gmail.com>
2018-03-20 15:19:46 -0400

    fix zsh completion

--- zsh.d/completions.zsh
+++ zsh.d/completions.zsh
@@ -5,5 +5,5 @@ if [ -e ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
     source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
     ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
-    bindkey '^ ' autosuggest-accept
+    bindkey '\t' autosuggest-accept
 fi
