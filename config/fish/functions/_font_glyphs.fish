a997dc7f21c15bc205aaa1dbb3ac11eca759de74
Nathan Anderson <andnat@gmail.com>
2018-12-04 09:34:32 -0500

    Add some nerdfont glyphs. Requires mononoki-nerd-font

---
+++ config/fish/functions/_font_glyphs.fish
@@ -0,0 +1,31 @@
+function _font_glyphs -S -d 'Define glyphs used by my shell'
+    # Powerline glyphs
+    set -x branch_glyph            \uE0A0
+    set -x right_black_arrow_glyph \uE0B0
+    set -x right_arrow_glyph       \uE0B1
+    set -x left_black_arrow_glyph  \uE0B2
+    set -x left_arrow_glyph        \uE0B3
+
+    # Additional glyphs
+    set -x detached_glyph          \u27A6
+    set -x tag_glyph               \u2302
+    set -x nonzero_exit_glyph      '! '
+    set -x superuser_glyph         '$ '
+    set -x bg_job_glyph            '% '
+    set -x hg_glyph                \u263F
+
+    set branch_glyph     \uF418
+    set detached_glyph   \uF417
+    set tag_glyph        \uF412
+
+    set git_dirty_glyph      \uF448 '' # nf-oct-pencil
+    set git_staged_glyph     \uF0C7 '' # nf-fa-save
+    set git_stashed_glyph    \uF0C6 '' # nf-fa-paperclip
+    set git_untracked_glyph  \uF128 '' # nf-fa-question
+    set git_ahead_glyph      \uF47B # nf-oct-chevron_up
+    set git_behind_glyph     \uF47C # nf-oct-chevron_down
+
+    set git_plus_glyph       \uF0DE # fa-sort-asc
+    set git_minus_glyph      \uF0DD # fa-sort-desc
+    set git_plus_minus_glyph \uF0DC # fa-sort
+  end
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,6 +2,16 @@
 #
 # Set the default prompt command.
 
+function __fish_prompt_duration
+  set --local duration (echo "$CMD_DURATION 1000" | awk '{printf "%.2fs", $1 / $2}')
+  set --local last (string split " " (echo $history[1]))
+
+  set_color 777
+  echo -n "  $last[1] took ~"
+  echo $duration
+  set_color normal
+end
+
 function __default_fish_prompt --description "Write out the prompt"
     set -l color_cwd
     set -l suffix
@@ -33,19 +43,25 @@ set gray (set_color -o black)
 # Fish git prompt
 set __fish_git_prompt_shorten_branch_len 10
 set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_describe_style branch
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '●'
-set __fish_git_prompt_char_stagedstate '✚'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+_font_glyphs
+set __fish_git_prompt_char_dirtystate $git_dirty_glyph
+set __fish_git_prompt_char_stagedstate $git_staged_glyph
+set __fish_git_prompt_char_untrackedfiles $git_untracked_glyph
+set __fish_git_prompt_char_stashstate $git_stashed_glyph
+set __fish_git_prompt_char_upstream_ahead $git_ahead_glyph
+set __fish_git_prompt_char_upstream_behind $git_behind_glyph
 
 function fish_prompt --description "Write out the prompt"
+  if test $CMD_DURATION -gt 1000
+    echo (__fish_prompt_duration)
+  end
   echo -n -s "$USER" @ (prompt_hostname) (prompt_pwd) (set_color normal) (__fish_git_prompt) "> "
 end
