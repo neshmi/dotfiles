d32eafe046e21a83607d75f6b169375ee2cec414
Nathan Anderson <andnat@gmail.com>
2021-06-03 17:12:33 -0400

    Clean up work related stuff from config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,13 @@
 set -U fish_greeting "🐟"
-functions -e open # turn off Fish's open command, it's not necessary on OS X
 
 # Add bits to path; once.
-for file_path in /usr/local/sbin $HOME/bin $HOME/go/bin /opt/bcdk/bin/ /opt/homebrew/bin
+for file_path in /usr/local/sbin $HOME/bin $HOME/go/bin /opt/homebrew/bin
   if not contains $file_path $fish_user_paths
     set -Ua fish_user_paths $file_path
   end
 end
 
-# Source private data
+# Source private data like credentials or work-related stuff
 if test -e $HOME/.private.config.fish
   source $HOME/.private.config.fish
 end
