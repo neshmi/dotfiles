34c038037cf6f280d09b1f9ca015dcb5f1218b01
Nathan Anderson <andnat@gmail.com>
2021-06-04 08:45:15 -0400

    Only alias if the new target commands exist

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,8 +1,8 @@
 if status --is-interactive
-    alias ping="prettyping --nolegend"
-    alias top="htop"
     abbr --add bi bundle install
     abbr --add be bundle exec
+    which prettyping > /dev/null && alias ping="prettyping --nolegend"
+    which htop > /dev/null && alias top="htop"
     abbr -a k kubectl
     abbr -a kg kubectl get
     abbr -a kgp kubectl get pods
