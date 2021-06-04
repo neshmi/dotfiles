32769cba3e1dd5b4a7188ab73af43743e86280f8
Nathan Anderson <andnat@gmail.com>
2021-06-04 08:45:58 -0400

    abbr definition consistency

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,8 +1,8 @@
 if status --is-interactive
-    abbr --add bi bundle install
-    abbr --add be bundle exec
     which prettyping > /dev/null && alias ping="prettyping --nolegend"
     which htop > /dev/null && alias top="htop"
+    abbr -a bi bundle install
+    abbr -a be bundle exec
     abbr -a k kubectl
     abbr -a kg kubectl get
     abbr -a kgp kubectl get pods
