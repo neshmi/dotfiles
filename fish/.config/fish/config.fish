0f568e10507f9ed16a0181a35617729e1b687404
Nathan Anderson <andnat@gmail.com>
2021-06-13 11:35:09 -0400

    Use fish_add_path builtin instead of the manual CAS

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,8 @@
 set -U fish_greeting "🐟"
 
 # Add bits to path; once.
-for file_path in /usr/local/sbin $HOME/bin $HOME/go/bin /opt/homebrew/bin
-  if not contains $file_path $fish_user_paths
-    set -Ua fish_user_paths $file_path
-  end
+for file_path in /usr/local/sbin $HOME/bin $HOME/go/bin /opt/homebrew/bin $HOME/.cargo/bin
+  fish_add_path $file_path
 end
 
 # Source private data like credentials or work-related stuff
